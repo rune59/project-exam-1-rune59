3369686240f42ccb1911ac6ae46086b6f3c1854c
rune59 <73398277+rune59@users.noreply.github.com>
2022-05-03 09:29:35 +0200

    ok

--- js/blogPostSpecific.js
+++ js/blogPostSpecific.js
@@ -17,13 +17,11 @@ async function fetchOneBlog() {
         const response = await fetch(url);
         const thisBlog = await response.json();
         console.log("thisBlog:" + thisBlog)
-        // createHTML(thisBlog);
 
         const mediaUrl = "http://localhost:10008/wp-json/wp/v2/media/" + thisBlog.featured_media;
         const mediaResponse = await fetch(mediaUrl);
         const thisMedia = await mediaResponse.json();
         console.log("this media:" + thisMedia)
-        // createHTML(thisBlog, thisMedia.media_details.sizes.full.source_url);
 
         createHTML(thisBlog, thisMedia.media_details.sizes.full.source_url);
 
@@ -36,17 +34,6 @@ async function fetchOneBlog() {
 }
 fetchOneBlog();
 
-
-// function createHTML(thisBlog) {
-//     blogSpecificContainer.innerHTML =
-//             `<div class="oneBlog">
-//                 <h2>Blog id: ${thisBlog.id}</h2>
-//                 <h2>Blog title: ${thisBlog.title.rendered}</h2>
-//                 <h3>Blog text: ${thisBlog.content.rendered}</h3>
-//                 <img src="${thisBlog._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}">
-//             </div>`
-//         ;
-// }
 
 function createHTML(thisBlog,image) {
     blogSpecificContainer.innerHTML =
@@ -70,14 +57,4 @@ function createHTML(thisBlog,image) {
 
 
 
-
-
-
-// function createHTML(thisBlog,image) {
-//     blogSpecificContainer.innerHTML =
-//             `<h2>${thisBlog.title.rendered}</h2>
-//             <img src="${image}">
-//             <h3>${thisBlog.content.rendered}</h3>
-//             `;
-// }
 
